e45601be2559a31bff7cc3e4bd11f40def40131b
EriEdF <24757505+EriEdF@users.noreply.github.com>
2021-02-17 20:48:47 +0100

    :hammer: edit main.py

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import sys
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sympy import symbols, solve
 from sklearn.linear_model import LinearRegression
 
+plt.style.use('default')
+plt.style.use('ggplot')
 
 def import_data(data_file):
     try:
@@ -25,14 +28,37 @@ print(df)
 
 # double reciprocal plot
 df = df.rdiv(1)
-
-x = np.array(df.cS).reshape((-1, 1))
+x = np.array(df.cS).reshape(-1, 1)
 y = np.array(df.v)
+
+# linear regression
 model = LinearRegression().fit(x, y)
 y_pred = model.predict(x)
 
-plt.scatter(x, y)
-plt.plot(x, y_pred)
+M = model.coef_[0]
+B = model.intercept_
+
+# solve equation to get 1/Km
+xvar = symbols('xvar')
+expr = M * xvar + B
+reci_Km = solve(expr)
+
+# no more reciprocals
+Km = 1 / abs(reci_Km[0])
+Vmax = 1 / abs(B)
+
+plt.plot(x, y_pred, color='k', label='Regression model')
+plt.scatter(x, y, edgecolor='k', facecolor='blue', alpha=0.5, label='Sample data')
+plt.plot(x, y_pred, color='k', label='Regression model')
+plt.xlabel("1 / [Substrate]")
+plt.ylabel("1 / Enzyme Activity")
+plt.legend(facecolor='white')
 plt.show()
 
+print('')
+print(f"y = {M}x + {B}")
+print(' ')
+print(f'Km: {Km}')
+print(f'Vmax: {Vmax}')
+print(' ')
 print("Coefficient of determination:", model.score(x, y))
